0183b5dd35a280e75ee592dbe2c6a740898a0d41
Maia Francine Endaya <maia.endaya@gmail.com>
2021-12-28 14:53:10 +0800

    added_bed_desk_closet

--- js/main.js
+++ js/main.js
@@ -7,27 +7,39 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //Bed
-const bedShape = new THREE.BoxGeometry(15, 3, 30);
-const bedMaterial = new THREE.MeshBasicMaterial({ color: 0x96616b });
+const bedShape = new THREE.BoxGeometry(12, 7, 19);
+const bedMaterial = new THREE.MeshBasicMaterial({ color: 0x37505c });
 const bed = new THREE.Mesh(bedShape, bedMaterial);
 scene.add(bed);
 
 //Desk
-const deskShape = new THREE.BoxGeometry(15, 12, 24);
-const deskMaterial = new THREE.MeshBasicMaterial({ color: 0x96616b });
+const deskShape = new THREE.BoxGeometry(17, 8, 8);
+const deskMaterial = new THREE.MeshBasicMaterial({ color: 0xddc8c4 });
 const desk = new THREE.Mesh(deskShape, deskMaterial);
 scene.add(desk);
 
-//Window
-
-//Door
-
 //Closet
+const closetShape = new THREE.BoxGeometry(12, 30, 6);
+const closetMaterial = new THREE.MeshBasicMaterial({ color: 0x96616b });
+const closet = new THREE.Mesh(closetShape, closetMaterial);
+scene.add(closet);
 
 camera.position.z = 35;
 
 function animate() {
   requestAnimationFrame(animate);
+
+  bed.position.x = 28;
+  bed.position.y = -8;
+  bed.position.z = 1;
+
+  desk.position.x = 10;
+  desk.position.y = -8;
+  desk.position.z = -9;
+
+
+  closet.position.x = -39;
+
   renderer.render(scene, camera);
 }
 animate();
