a31b424c40e5bdc6de0706475093e44a0c6ca3eb
Maia Francine Endaya <maia.endaya@gmail.com>
2021-12-28 15:11:01 +0800

    added_door_and_window

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//Furnitures
 //Bed
 const bedShape = new THREE.BoxGeometry(12, 7, 19);
 const bedMaterial = new THREE.MeshBasicMaterial({ color: 0x37505c });
@@ -24,11 +25,25 @@ const closetMaterial = new THREE.MeshBasicMaterial({ color: 0x96616b });
 const closet = new THREE.Mesh(closetShape, closetMaterial);
 scene.add(closet);
 
+//Doors and Windows
+//Door
+const doorPlane = new THREE.PlaneGeometry(12, 23);
+const doorMaterial = new THREE.MeshBasicMaterial({ color: 0x693d41, side: THREE.DoubleSide });
+const door = new THREE.Mesh(doorPlane, doorMaterial);
+scene.add(door);
+
+//Window
+const windowPlane = new THREE.PlaneGeometry(18, 13);
+const windowMaterial = new THREE.MeshBasicMaterial({ color: 0x34a8eb, side: THREE.DoubleSide });
+const roomWindow = new THREE.Mesh(windowPlane, windowMaterial);
+scene.add(roomWindow);
+
 camera.position.z = 35;
 
 function animate() {
   requestAnimationFrame(animate);
 
+  //Furniture
   bed.position.x = 28;
   bed.position.y = -8;
   bed.position.z = 1;
@@ -37,8 +52,16 @@ function animate() {
   desk.position.y = -8;
   desk.position.z = -9;
 
-
   closet.position.x = -39;
+
+  //Doors and Windows
+  door.position.x = -25;
+  door.position.z = -10;
+
+  roomWindow.position.x = 10;
+  roomWindow.position.y = 5;
+  roomWindow.position.z = -10;
+
 
   renderer.render(scene, camera);
 }
